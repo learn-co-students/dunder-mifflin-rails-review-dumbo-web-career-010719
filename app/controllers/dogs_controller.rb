48d7d66b644cf95ac48ec6706f1fa6f163a529e7
NorbertLam <noobertlm@gmail.com>
2019-02-05 13:39:39 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    @employees = Employee.all
+  end
+  
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
