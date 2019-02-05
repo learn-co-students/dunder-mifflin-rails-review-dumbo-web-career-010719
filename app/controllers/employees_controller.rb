5fefd4b5cb2136dca15ea303e362268841ce2f6f
rchow1944 <rchow2@buffalo.edu>
2019-02-05 14:02:19 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+  def index
+    #code
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    #code
+    @employee = Employee.new
+  end
+
+  def create
+    #code
+    # byebug
+    @employee = Employee.new(employee_params)
+    if @employee.valid?
+      @employee.save
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    #code
+    @employee = Employee.find(params[:id])
+    if @employee.update(employee_params)
+      redirect_to employee_path(@employee)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
 end
