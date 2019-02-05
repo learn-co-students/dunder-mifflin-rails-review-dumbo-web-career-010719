aa75f937f6f02d886f9a5a253455455ad8adb339
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-02-05 17:28:17 -0500

    finish

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,57 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees=Employee.all
+        render :index
+    end
+
+    def show
+        @employee=Employee.find(params[:id])
+        render :show
+    end
+
+    def new
+        @employee=Employee.new
+        render :new
+    end
+
+    def create
+        @employee=Employee.new(employee_params)
+        if @employee.valid?
+            @employee.save
+            redirect_to employee_path(@employee)
+        else
+            flash[:error]=[]
+            flash[:error]=employee.errors.full_messages
+            @employee=Employee.new
+            render :new
+        end
+    end
+
+    def edit
+        @employee=Employee.find(params[:id])
+    end
+
+    def update
+        employee=Employee.find(params[:id])
+        employee.update(employee_params)
+        if employee.valid?
+            redirect_to employee_path(employee)
+        else
+            flash[:error]=[]
+            flash[:error]=employee.errors.full_messages
+            @employee=Employee.find(params[:id])
+            render :edit
+        end
+    end
+
+    def destroy
+        Employee.find(params[:id]).destroy
+        redirect_to employees_path
+    end
+
+    private
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :dogs
+  resources :dogs, only: [:index, :show]
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get "/dogs?sort=asc", to: "dogs#index", as: 'sort_dog'
 end
