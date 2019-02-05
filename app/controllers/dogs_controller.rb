487809cdbfd0d5de2b4a6f0caa8d6d2c8b25bc76
Elizabeth Le <elizabethlemk@yahoo.com>
2019-02-05 18:46:56 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,37 @@
 class DogsController < ApplicationController
+  before_action :set_dogs!, only: [:show, :edit, :update, :destroy]
+  def index
+    @dogs = Dog.all
+    @dogs = @dogs.sort_by {|dog| dog.employees.count}
+    render :index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to dogs_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
+
+  def set_dogs!
+    @dog= Dog.find(params[:id])
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,53 @@
 class EmployeesController < ApplicationController
+  before_action :set_employees!, only: [:show, :edit, :update, :destroy]
+  def index
+    @employees= Employee.all
+  end
+
+  def show
+  end
+
+  def new
+    flash[:error] = nil
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      flash[:error] = @employee.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    flash[:error] = nil
+  end
+
+  def update
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      flash[:error] = @employee.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to employees_path
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+  end
+
+  def set_employees!
+    @employee = Employee.find(params[:id])
+  end
 end
